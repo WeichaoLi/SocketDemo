e2098d2906a0ddb30f7fc38bc5d1356aa4952b43
WeichaoLi <1303169452@qq.com>
2016-02-24 14:58:55 +0800

    Initial Commit

---
+++ SocketDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocketDemo
+//
+//  Created by 李伟超 on 16/2/24.
+//  Copyright © 2016年 LWC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
